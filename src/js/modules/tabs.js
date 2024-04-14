1eacc077736e4c9bc899c22df8a1c2f0bfec7031
Ilya <ilieturcan424@gmail.com>
2024-04-14 10:09:49 +0300

    first commit

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,24 +1,24 @@
 "use strict";
 
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
-    const header = document.querySelector(headerSelector),
-        tab = document.querySelectorAll(tabSelector),
-        content = document.querySelectorAll(contentSelector),
-        active = document.querySelector(activeClass);
+	const header = document.querySelector(headerSelector),
+		tab = document.querySelectorAll(tabSelector),
+		content = document.querySelectorAll(contentSelector),
+		active = document.querySelector(activeClass);
 
-    function hideTabContent() {
-        content.forEach(item => {
-            item.style.display = "none";
-        });
-    }
+	function hideTabContent() {
+		content.forEach(item => {
+			item.style.display = "none";
+		});
+	}
 
-    function showTabContent(i) {
-        content[i].style.display = "block";
-    }
-
-    tab.forEach(item => {
-        item.classList.remove(activeClass);
-    });
+	function showTabContent(i) {
+		content[i].style.display = "block";
+	}
+	
+	tab.forEach(item => {
+		item.classList.remove(activeClass);
+	});
 };
 
 export default tabs;
