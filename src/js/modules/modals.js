20202a5715be03bb35d312ea7400870c30cf6769
Ilya <ilieturcan424@gmail.com>
2024-05-07 14:58:52 +0300

    Add modals functionality v0.0.1
    Added listeners on buttons and added functionality of closing all modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,34 +1,42 @@
 const modals = () => {
 
-	function bindModal(openSelector, modalSelector, closeSelector) {
+	function bindModal(openSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 		const open = document.querySelectorAll(openSelector),
 			modal = document.querySelector(modalSelector),
-			close = document.querySelector(closeSelector);
+			close = document.querySelector(closeSelector),
+			windows = document.querySelectorAll("[data-modal]");
 
 		open.forEach(item => {
 			item.addEventListener("click", () => {
-				modal.classList.add("show");
+				closeAllModals();
+				modal.style.display = "block";
 				document.body.classList.add("modal-open");
 			});
 		});
 
 		close.addEventListener("click", () => {
-			modal.classList.remove("show");
+			closeAllModals();
 			document.body.classList.remove("modal-open");
 		});
 
 		modal.addEventListener("click", (e) => {
-			if (e.target == modal) {
-				modal.classList.remove("show");
+			if (e.target == modal && closeClickOverlay == true) {
+				closeAllModals();
 				document.body.classList.remove("modal-open");
 			}
 		});        
+
+		function closeAllModals() {
+			windows.forEach(item => {
+				item.style.display = "none";
+			});
+		}
 	}
 
 	function showModalByTime(time, selector) {
 		const modal = document.querySelector(selector);
 		setTimeout(() => {
-			modal.classList.add("show");
+			modal.style.display = "block";
 			document.body.classList.add("modal-open");
 		}, time);
 	}
@@ -36,6 +44,8 @@ const modals = () => {
 	bindModal(".header_btn", ".popup_engineer", ".popup_engineer .popup_close");
 	bindModal(".phone_link", ".popup", ".popup .popup_close");
 	bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+	bindModal(".popup_calc_button", ".popup_calc_profile", ".popup_calc_profile_close", false);
+	bindModal(".popup_calc_profile_button", ".popup_calc_end", ".popup_calc_end_close", false);
 	// showModalByTime(60000, ".popup");
 };
 
