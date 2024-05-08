be01f1c48a911dd81619749c5710f739f94b0ee7
Ilya <ilieturcan424@gmail.com>
2024-05-08 16:28:49 +0300

    Add changeModalState file to modules

---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,7 @@
+const changeModalState = () => {
+	const windowForm = document.querySelectorAll(".balcon_icons_img"),
+		windowWidth = document.querySelector("#width"),
+		windowsHeight = document.querySelector("#height");
+};
+
+export default changeModalState;
