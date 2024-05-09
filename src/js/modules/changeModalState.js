02739dc19dcb5cc4e78beab03ab0b7fa5e155a12
Ilya <ilieturcan424@gmail.com>
2024-05-09 16:24:44 +0300

    Add changeModalState v0.1.0

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -2,20 +2,30 @@ import checkNumInputs from "./checkNumInputs";
 
 const changeModalState = (state) => {
 	const windowForm = document.querySelectorAll(".balcon_icons_img"),
-		windowWidth = document.querySelector("#width"),
-		windowHeight = document.querySelector("#height"),
+		windowWidth = document.querySelectorAll("#width"),
+		windowHeight = document.querySelectorAll("#height"),
 		windowType = document.querySelector(".view_type"),
 		windowProfile = document.querySelectorAll(".checkbox");
 	
 	checkNumInputs("#height");
 	checkNumInputs("#width");
 	
-	windowForm.forEach((item, i) => {
-		item.addEventListener("click", () => {
-			state.form = i;
-			console.log(state);
+	function bindActionToElems(event, elem, prop) {
+		elem.forEach((item, i) => {
+			item.addEventListener(event, () => {
+				if (elem.length > 1) {
+					state[prop] = i;
+				} else {
+					state[prop] = item.value;
+				}
+				console.log(state);
+			});
 		});
-	});
+	}
+
+	bindActionToElems("click", windowForm, "form");
+	bindActionToElems("input", windowWidth, "width");
+	bindActionToElems("input", windowHeight, "height");
 };
 
 export default changeModalState;
